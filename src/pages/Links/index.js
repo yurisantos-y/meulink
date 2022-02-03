f11c1d3904751b5595220249d649ff222c41cef3
Yuri <yuri01.sp@gmail.com>
2022-02-02 22:22:38 -0300

    Second commit

--- src/pages/Links/index.js
+++ src/pages/Links/index.js
@@ -1,13 +1,15 @@
 import './links.css';
 import { FiArrowLeft, FiLink, FiTrash } from 'react-icons/fi';
-
+import { Link } from 'react-router-dom'
 
 export default function Links(){
     return(
       <div className="links-container">
        
        <div className="links-header">
-        <FiArrowLeft size={38} color="#FFF"/>
+         <Link to="/">
+         <FiArrowLeft size={38} color="#FFF"/>
+         </Link>
         <h1>Meus Links</h1>
        </div>
 
@@ -16,10 +18,21 @@ export default function Links(){
           <FiLink size={18} color="#FFF"/>
           https://sujeitoprogramador.com
         </button>
-        <button>
+        <button className="link-delete">
           <FiTrash size={24} color="#FF5454"/>
         </button>
        </div>
+
+       <div className="links-item">
+        <button className='link'>
+          <FiLink size={18} color="#FFF"/>
+          https://sujeitoprogramador.com
+        </button>
+        <button className="link-delete">
+          <FiTrash size={24} color="#FF5454"/>
+        </button>
+       </div>
+
       </div>
     )
   }
